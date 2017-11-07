3dc710c58b938129aff1d9f5997ce126f9886532
marta <martaseguraaymerich@gmail.com>
2017-11-07 16:25:49 +0100

    Add logo png

---
+++ ejercicio20.c
@@ -0,0 +1,22 @@
+/*
+20. Write a calculator program. Program flow should be as follow:
+1) Ask for a number (float)
+2) Ask for desired operation: '+', '-', '*', '/'
+3) Ask for another number (float)
+4) Show operation and results
+5) Go to step 2)
+*/
+
+/*1*/
+
+#include <stdio.h>
+
+#include <math.h>
+
+int main()
+{
+    float num = 0;
+    float result = 0;
+}
+
+
