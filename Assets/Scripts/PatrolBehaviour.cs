04948b6a9a86d0fcf4fa433d5f5b97800e7f7528
Marta <martaseguraaymerich@gmail.com>
2017-12-07 18:31:48 +0100

    Add scripts PatrolBehaviour and PlatformTrigger
    Add scripts PatrolBehaviour and PlatformTrigger on TestingScene

---
+++ Assets/Scripts/PatrolBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatrolBehaviour : MonoBehaviour {
+
+    public Vector2 direction;
+    public float speed;
+    public float timePatrol;
+    public float timeCounter;
+
+    // Use this for initialization
+    void Start()
+    {
+        timeCounter = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+
+        if(timeCounter >= timePatrol)
+        {
+            speed *= -1;
+            timeCounter = 0;
+        }
+        else timeCounter += Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/PlatformTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.SetParent(this.transform);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            other.transform.SetParent(null);
+        }
+    }
+}
