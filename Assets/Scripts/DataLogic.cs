355e069a227e479f2d521a35883494fbfd36bf80
Albert <campillo.boixet.albert@gmail.com>
2017-12-15 18:46:59 +0100

    Language scripts

---
+++ Assets/Scripts/DataLogic.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataLogic : MonoBehaviour
+{
+	// Use this for initialization
+	void Awake ()
+    {      
+        Language.Initialize();        
+	}
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            Language.SetLanguage(Language.Lang.esES);
+        }
+        if(Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            Language.SetLanguage(Language.Lang.enUS);
+        }
+    }
+}
