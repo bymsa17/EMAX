f7dfb95017f790099f8c6b9769831f79d8c9c76f
Marta <martaseguraaymerich@gmail.com>
2017-12-17 15:14:54 +0100

    Create endinScene and logoAnimation
    Create endinScene and logoAnimation

--- Assets/Scripts/EndingButtons.cs
+++ Assets/Scripts/EndingButtons.cs
@@ -5,25 +5,36 @@ using UnityEngine.SceneManagement;
 
 public class EndingButtons : MonoBehaviour
 {
+
+    public PlayerBehaviour player;
+
 	// Use this for initialization
 	void Start ()
     {
-		
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
-		
+		if (player.life <= 0)
+        {
+            ReloadLevel(5);
+        }
 	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("COLLISION");
+            ReloadLevel(4);
+        }
+    }
 
     public void ReloadLevel(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex);
         //player.Reset();
-    }
-    public void NextLevel(int buildIndex)
-    {
-        SceneManager.LoadScene(buildIndex);
     }
 }
