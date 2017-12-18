06340a7aaac991e21d2238e075527e49ffead701
Fokus <campillo.boixet.albert@gmail.com>
2017-12-18 03:51:47 +0100

    Merge

---
+++ Assets/Scripts/DeadTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadTrigger : MonoBehaviour {
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("KillPlayer");
+            SceneManager.LoadScene(3); 
+        }
+    }
+}
