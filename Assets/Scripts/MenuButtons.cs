7a1ccb76efc1050ca12affad9c8d74377e6b0d1a
Marta <martaseguraaymerich@gmail.com>
2017-12-18 02:24:28 +0100

    Update PausaButtons and create script PausaDetection
    Update PausaButtons and create script PausaDetection

--- Assets/Scripts/MenuButtons.cs
+++ Assets/Scripts/MenuButtons.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtons : MonoBehaviour
 {
+    public GameObject canvasGameplay;
+
     public void LoadScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex);
+        if(Time.timeScale == 0) Time.timeScale = 1;
     }
 
     public void LoadScene(string nameScene)
---
+++ Assets/Scripts/PauseDetection.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseDetection : MonoBehaviour
+{
+    public GameObject canvasGameplay;
+
+    public void LoadScene(int buildIndex)
+    {
+        SceneManager.LoadScene(buildIndex);
+        if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+            canvasGameplay.SetActive(true);
+        }
+        else canvasGameplay.SetActive(true);
+    }
+}
