39eb075ccf96adf6ccbd1cddc5237b99a0fdc9d0
Fokus <campillo.boixet.albert@gmail.com>
2017-12-18 03:51:41 +0100

    Merge

--- Assets/Scripts/EndingButtons.cs
+++ Assets/Scripts/EndingButtons.cs
@@ -5,36 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class EndingButtons : MonoBehaviour
 {
-
-    public PlayerBehaviour player;
-
-	// Use this for initialization
-	void Start ()
-    {
-        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
-		if (player.life <= 0)
-        {
-            ReloadLevel(5);
-        }
-	}
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.tag == "Player")
-        {
-            Debug.Log("COLLISION");
-            ReloadLevel(4);
-        }
-    }
-
-    public void ReloadLevel(int buildIndex)
+    public void LoadScene(int buildIndex)
     {
         SceneManager.LoadScene(buildIndex);
-        //player.Reset();
     }
 }
