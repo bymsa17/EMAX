345fe3e4515a4802c47692fe82323631e3547e37
Marta <martaseguraaymerich@gmail.com>
2018-01-20 22:41:21 +0100

    Create temporary punctuation script
    Create temporary punctuation script

---
+++ Assets/Scripts/Punctuation.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Punctuation : MonoBehaviour
+{
+    public float time;
+    public int ability;
+    public int deaths;
+    public bool piece;
+
+    public float score;
+    public float maxScore;
+
+	void Start ()
+    {
+        score = 0;
+        maxScore = 100;
+	}
+	
+
+	void Update ()
+    {
+        if(time <= 60)
+        {
+            score += 20;
+        }
+
+        if(ability <= 8)
+        {
+            score += 20;
+        }
+        else if ((ability > 8) || (ability <= 10))
+        {
+            score += 10;
+        }
+        else if(ability > 10)
+        {
+            score += 0;
+        }
+
+        if(deaths == 0)
+        {
+            score += 20;
+        }
+        else if(deaths == 1)
+        {
+            score += 15;
+        }
+        else if(deaths == 2)
+        {
+            score += 10;
+        }
+        else if(deaths == 3)
+        {
+            score += 0;
+        }
+
+        if(piece == true)
+        {
+            score += 40;
+        }
+        else score += 0;
+
+	}
+}
