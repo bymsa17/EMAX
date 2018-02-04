9355fc89da42ebe5a17ff52b6831c543698da147
Marta <martaseguraaymerich@gmail.com>
2018-02-04 23:11:27 +0100

    Update script TrunkBehaviour
    Update script TrunkBehaviour

--- Assets/Scripts/Behaviours/TrunkBehaviour.cs
+++ Assets/Scripts/Behaviours/TrunkBehaviour.cs
@@ -5,40 +5,66 @@ using UnityEngine;
 public class TrunkBehaviour : MonoBehaviour
 {
     public Transform trunk;
-    public float speed;
+
     public float maxDistance;
     public bool active;
     public float startDelay;
 
-    // Use this for initialization
+    public Vector3 iniPos;
+    public Vector3 finalPos;
+    public float currentTime;
+    public float timeDuration;
+
     void Start ()
     {
         active = false;
-        maxDistance = this.transform.localPosition.y + maxDistance;
+        //maxDistance = this.transform.localPosition.y + maxDistance;
+
+        iniPos = transform.localPosition;
+        finalPos = new Vector3 (transform.localPosition.x, iniPos.y + maxDistance, transform.localPosition.z);
     }
 	
-	// Update is called once per frame
 	void Update ()
     {
         if (active)
         {
             UpTrunk();
-            /*
-            if (startDelay > 0)
-            {
-                startDelay -= Time.deltaTime;
-                return;
-            }*/
         }
 	}
 
+    public static float BackEaseOut(float t, float b, float c, float d)
+    {
+        return c * ((t = t / d - 1) * t * ((1.70158f + 1) * t + 1.70158f) + 1) + b;
+    }
+
     public void UpTrunk()
     {
-        transform.localPosition += new Vector3(0, speed, 0) * Time.deltaTime;
-
-        if(transform.localPosition.y >= maxDistance)
+        if(currentTime <= timeDuration)
         {
-            speed = 0;
+            float value = BackEaseOut(currentTime, iniPos.y, finalPos.y - iniPos.y, timeDuration);
+
+            currentTime += Time.deltaTime;
+
+            transform.localPosition = new Vector3(transform.localPosition.x, value, transform.localPosition.z);
+
+            if(currentTime >= timeDuration)
+            {
+                transform.localPosition = new Vector3(transform.localPosition.x, finalPos.y, transform.localPosition.z);
+            }
+        }
+
+        if(transform.localPosition.y == finalPos.y)
+        {
+            startDelay -= Time.deltaTime;
+
+            if(startDelay < 0)
+            {
+                startDelay = 0;
+                currentTime = 0;
+                Vector3 ini = iniPos;
+                iniPos = finalPos;
+                finalPos = ini;
+            }
         }
     }
 }
