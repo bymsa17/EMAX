df079e15277d8d81841bac60e7053983d9f08f11
Marta <martaseguraaymerich@gmail.com>
2018-02-07 19:04:01 +0100

    Create trigger and add scripts to trunks
    Create trigger and add scripts to trunks

--- Assets/Scripts/Triggers/TrunkTrigger.cs
+++ Assets/Scripts/Triggers/TrunkTrigger.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class TrunkTrigger : MonoBehaviour
 {
-    public TrunkBehaviour trunk;
+    public TrunkBehaviour trunk01;
+    public TrunkBehaviour trunk02;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             Debug.Log("TrunkTrigger");
-            trunk.active = true;
+            trunk01.active = true;
+            trunk02.active = true;
         }
     }
 }
