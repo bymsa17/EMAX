9eb72180c538e94465f58c0002811a61ba3f3f0a
Marta <martaseguraaymerich@gmail.com>
2018-02-07 18:32:33 +0100

    Create checkpoint scripts
    Create scripts CheckpointBehaviour and CheckpointTrigger

---
+++ Assets/Scripts/Behaviours/CheckpointBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointBehaviour : MonoBehaviour
+{
+    public ParticleSystem particles;
+    public bool active;
+
+    void Start ()
+    {
+        active = false;
+        particles.Pause();
+    }
+	
+	void Update ()
+    {
+        if(active)
+        {
+            ActiveCheckpoint();
+        }
+	}
+
+    public void ActiveCheckpoint()
+    {
+        particles.Play();
+    }
+}
