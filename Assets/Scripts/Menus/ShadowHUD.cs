eb53c380f6680304d472a24c8de5d4887c5c3081
Marta <martaseguraaymerich@gmail.com>
2018-02-16 04:47:32 +0100

    Create script ShadowHUD
    Create script ShadowHUD

---
+++ Assets/Scripts/Menus/ShadowHUD.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowHUD : MonoBehaviour
+{
+    public GameObject levelStart;
+    public GameObject levelEnd;
+    public GameObject shadow;
+    public float resta;
+    public float canvasSize;
+    public float shadowPosition;
+    public RectTransform rectTransform;
+
+    void Start()
+    {
+        resta = levelEnd.transform.position.x - levelStart.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        shadowPosition = ((shadow.transform.position.x - levelStart.transform.position.x) / resta * canvasSize);
+        if(shadowPosition < 0)
+        {
+            shadowPosition = 0;
+        }
+        if(shadowPosition > resta)
+        {
+            shadowPosition = resta;
+        }
+
+        rectTransform.offsetMax = new Vector2(shadowPosition + 10, 10);
+        rectTransform.offsetMin = new Vector2(shadowPosition + 10, -10);
+
+    }
+}
