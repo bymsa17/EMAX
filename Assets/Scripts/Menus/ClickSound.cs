b31e7fc5bc4e3e346b631b4648a59dffaba7ae22
Fokus <campillo.boixet.albert@gmail.com>
2018-02-16 04:47:31 +0100

    Intento de hacer algo de programación
    Marta arreglalo xD

---
+++ Assets/Scripts/Menus/ClickSound.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSound : MonoBehaviour {
+
+    private AudioPlayer audioPlayer;
+    // Use this for initialization
+    void Start()
+    {
+        audioPlayer = GetComponentInChildren<AudioPlayer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton()
+        {
+            audioPlayer.PlaySFX(0, 1, Random.Range(0.9f, 1.1f));
+        }
+      
+    }
+}
