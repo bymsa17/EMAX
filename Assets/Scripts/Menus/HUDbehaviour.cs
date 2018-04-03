99d74173de65ea704c300ea19061427364cfa2ef
Albert Campillo Boixet <campillo.boixet.albert@gmail.com>
2018-04-03 17:04:03 +0200

    Animacion habilidad HUD
    Falta la programación

---
+++ Assets/Scripts/Menus/HUDbehaviour.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDbehaviour : MonoBehaviour {
+
+    public bool portal = false;
+
+    void Portal()
+    {
+        if (portal == true) anim.SetBool("portalGet", true);
+        else anim.SetBool("portalNull", true);
+    }
+}
