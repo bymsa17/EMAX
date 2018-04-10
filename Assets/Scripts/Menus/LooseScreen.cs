7ca5ae2aabf462a9141ab5f57469ce3d9e091047
Fokus <campillo.boixet.albert@gmail.com>
2018-04-10 21:34:16 +0200

    Fix anim lose

---
+++ Assets/Scripts/Menus/LooseScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseScreen : MonoBehaviour
+{
+
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("loseAnim", true);
+    }
+}
