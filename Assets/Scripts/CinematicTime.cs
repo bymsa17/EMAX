3fe26055f9870dfb9001691425121e98592399e6
Marta <martaseguraaymerich@gmail.com>
2018-04-12 02:34:09 +0200

    Create cinematic scene and script CinematicTime
    Create cinematic scene and script CinematicTime

---
+++ Assets/Scripts/CinematicTime.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CinematicTime : MonoBehaviour
+{
+    public SceneTransition transition;
+    public GameObject text;
+    float currentTime;
+    float timeDuration;
+
+    void Start()
+    {
+        timeDuration = 5;
+    }
+
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= timeDuration)
+        {
+            text.SetActive(true);
+        }
+
+        if((Input.GetKeyDown(KeyCode.Return)) && (currentTime >= timeDuration))
+        {
+            transition.LoadScene(4);
+        }
+    }
+}
