31593ce9a268baf122da75b9ca36e4179e47f410
Marta <martaseguraaymerich@gmail.com>
2018-04-12 03:16:23 +0200

    Create scripts PieceBehaviour and PieceTrigger
    Create scripts PieceBehaviour and PieceTrigger

---
+++ Assets/Scripts/Behaviours/PieceBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceBehaviour : MonoBehaviour
+{
+    public Transform piece;
+    public float angularV;
+
+    private void Start()
+    {
+        piece = this.transform;
+    }
+
+    private void Update()
+    {
+        piece.Rotate(Vector3.forward, angularV * Time.deltaTime);
+    }
+}
