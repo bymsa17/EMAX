8d2a1d017f2efcccebef73f30c9f9658e2585411
Marta <martaeguraaymerich@gmail.com>
2018-04-26 16:16:46 +0200

    Create script HideMouse and ShowMouse
    Create script HideMouse and ShowMouse

---
+++ Assets/Scripts/HideMouse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideMouse : MonoBehaviour
+{
+    void Start()
+    {
+        Hide();
+    }
+
+    public void Hide()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+    }
+}
---
+++ Assets/Scripts/ShowMouse.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMouse : MonoBehaviour
+{
+	void Start ()
+    {
+        Show();
+	}
+
+    public void Show()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
