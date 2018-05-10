c2ac2bb1e902b8be24a3d6c346965c3de56db8a5
Marta <martaseguraaymerich@gmail.com>
2018-05-10 17:06:33 +0200

    Create script ButtonTrigger and correct level scene
    Create script ButtonTrigger and correct level scene

---
+++ Assets/Scripts/Triggers/ButtonTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public bool isActive;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Player")
+        {
+            Debug.Log("ButtonTrigger");
+            isActive = true;
+        }
+    }
+}
