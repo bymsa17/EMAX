631a5b74377f71d08acc4db873f16a477eb1eab8
Marta <martaseguraaymerich@gmail.com>
2018-05-10 16:37:12 +0200

    Create script EnemyBehaviour
    Create script EnemyBehaviour

---
+++ Assets/Scripts/Behaviours/EnemyBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public Transform enemy;
+    public float angularV;
+
+    void Start ()
+    {
+        enemy = this.transform;
+    }
+
+    void Update ()
+    {
+
+        enemy.Rotate(Vector3.forward, angularV * Time.deltaTime);
+    }
+}
