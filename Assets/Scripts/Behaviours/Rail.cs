5414f966a080191c2fbf3ba05d5ac9c64a6237dc
Albert Campillo Boixet <campillo.boixet.albert@gmail.com>
2018-05-18 00:36:27 +0200

    Pruebas shadow gameplay

---
+++ Assets/Scripts/Behaviours/Rail.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour {
+
+	[SerializeField]
+	Transform[] waypoints;
+
+	[SerializeField]
+	float moveSpeed = 0.025f;
+
+	int waypointIndex = 0;
+
+	void Start () {
+		transform.position = waypoints [waypointIndex].transform.position;
+	}
+
+	void Update () {
+		Move ();
+	}
+
+	void Move()
+	{
+		transform.position = Vector2.MoveTowards (transform.position,
+												waypoints[waypointIndex].transform.position,
+												moveSpeed * Time.deltaTime);
+
+		if (transform.position == waypoints [waypointIndex].transform.position) {
+			waypointIndex += 1;
+		}
+				
+		if (waypointIndex == waypoints.Length)
+			waypointIndex = 0;
+	}
+
+}
