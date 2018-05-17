f51fa22744be57d3ec7d8dfe4a2c28c3f3a12a8c
Albert Campillo Boixet <campillo.boixet.albert@gmail.com>
2018-05-17 17:33:39 +0200

    Prueba Anim Transition

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transition : MonoBehaviour
+{
+
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("Trans", true);
+    }
+}
