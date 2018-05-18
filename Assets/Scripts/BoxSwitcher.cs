e67309ad0626feb64700d1480c95a2bc777bf30a
Marta <martaseguraaymerich@gmail.com>
2018-05-18 23:07:14 +0200

    Correct script BoxSwitcher and add animations pieces
    Correct script BoxSwitcher and add animations pieces

--- Assets/Scripts/BoxSwitcher.cs
+++ Assets/Scripts/BoxSwitcher.cs
@@ -7,49 +7,33 @@ public class BoxSwitcher : MonoBehaviour
     public GameObject unloadBox;
     public GameObject loadBox;
     public GameObject pieza1;
+    public Animator anim1;
     public GameObject pieza2;
+    public Animator anim2;
     public GameObject pieza3;
+    public Animator anim3;
     public PlayerBehaviour player;
-    //public float timeCounter;
-    //public bool isActive;
 
     void Start ()
     {
         loadBox.SetActive(false);
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehaviour>();
 	}
-    /*
-    public void Update()
-    {
-        if(isActive == true)
-        {
-            timeCounter++;
-            ActivePieces();
-        }
-    }*/
 
     public void Switch()
     {
         unloadBox.SetActive(false);
         loadBox.SetActive(true);
 
-        //isActive = true;
-
         Vector2 dir = loadBox.transform.position - player.transform.position;
         dir.Normalize();
         pieza1.GetComponent<Rigidbody2D>().AddForce(dir * 1000, ForceMode2D.Impulse);
         pieza2.GetComponent<Rigidbody2D>().AddForce(dir * 1000, ForceMode2D.Impulse);
         pieza3.GetComponent<Rigidbody2D>().AddForce(dir * 1000, ForceMode2D.Impulse);
+
+        anim1.SetTrigger("DestroyPiece");
+        anim2.SetTrigger("DestroyPiece");
+        anim3.SetTrigger("DestroyPiece");
+        Destroy(loadBox, 2);
     }
-    /*
-    public void ActivePieces()
-    {
-        if(timeCounter >= 300)
-        {
-            Debug.Log("desactivePieces");
-            pieza1.SetActive(false);
-            pieza2.SetActive(false);
-            pieza3.SetActive(false);
-        }
-    }*/
 }
