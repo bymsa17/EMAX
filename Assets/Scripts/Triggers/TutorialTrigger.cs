4c28f226bd3cdd16db8f1483c766a0bbcd19f356
bymsa17 <martaseguraaymerich@gmail.com>
2018-05-24 17:02:11 +0200

    Add script TutorialTrigger and TutorialCanvas
    Add script TutorialTrigger and TutorialCanvas

---
+++ Assets/Scripts/Triggers/TutorialTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public GameObject Text;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Text.SetActive(true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Text.SetActive(false);
+    }
+}
