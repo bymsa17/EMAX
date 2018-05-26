e7dd5dd30a079433c8892c52cb62207ea2b95899
Marta <martaseguraaymerich@gmail.com>
2018-05-26 04:15:51 +0200

    Correct script ElevatorTrigger, add elevator animations
    Correct script ElevatorTrigger, add elevator animations and correct level scene

--- Assets/Scripts/Triggers/ElevatorTrigger.cs
+++ Assets/Scripts/Triggers/ElevatorTrigger.cs
@@ -5,13 +5,19 @@ using UnityEngine;
 public class ElevatorTrigger : MonoBehaviour
 {
     public ElevatorBehaviour elevator;
+    public Animator animElevator;
+    public Animator animDoorL;
+    public Animator animDoorR;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
             Debug.Log("ElevatorTrigger");
-            elevator.activeDown = true;
+            //elevator.activeDown = true;
+            animElevator.SetTrigger("UpElevator");
+            animDoorL.SetBool("OpenDoor", true);
+            animDoorR.SetBool("OpenDoor", true);
         }
     }
 }
