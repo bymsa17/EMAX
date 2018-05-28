20066c4a2c6cd21b91e4e15e2783719a748185ab
Marta <martaseguraaymerich@gmail.com>
2018-05-28 19:46:28 +0200

    Correct canvas logo
    Correct canvas logo

--- Assets/Scripts/Transition.cs
+++ Assets/Scripts/Transition.cs
@@ -1,19 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Transition : MonoBehaviour
 {
+    float currentTime;
+    float fadeTime = 2.0f;
+    int sceneToLoad = 0;
+    public Animator animScreen;
 
-    public Animator anim;
+    bool fadeOut = false;
 
     void Start()
     {
-        anim = GetComponent<Animator>();
+        FadeIn();
     }
 
     void Update()
     {
-        anim.SetBool("Trans", true);
+        if(!fadeOut) return;
+
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= fadeTime + .1f)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+
+    public void LoadScene(string title)
+    {
+        SceneManager.LoadScene(title);
+    }
+
+    void FadeIn()
+    {
+        animScreen.SetBool("Transition", true);
+    }
+    void FadeOut()
+    {
+        //animScreen.SetTrigger("Transition");
+        fadeOut = true;
+    }
+
+    public void LoadScene(int n)
+    {
+        sceneToLoad = n;
+        FadeOut();
     }
 }
