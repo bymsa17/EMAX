6b3990a4c08f5981c4aae262459855c3ff39f9f3
Albert Campillo Boixet <campillo.boixet.albert@gmail.com>
2018-05-31 03:01:58 +0200

    Fix redirection endPoint

--- Assets/Scripts/EndingConditions.cs
+++ Assets/Scripts/EndingConditions.cs
@@ -35,7 +35,7 @@ public class EndingConditions : MonoBehaviour
         if(other.tag == "Player")
         {
             Debug.Log("COLLISION");
-            ReloadLevel(5);
+            ReloadLevel(4);
         }
     }
 
