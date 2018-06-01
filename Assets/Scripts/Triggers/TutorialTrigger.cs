1460294fc24a78da517f21079c60844fdbaae4a9
Marta <martaseguraaymerich@gmail.com>
2018-06-01 06:46:42 +0200

    Add sound tutorial

--- Assets/Scripts/Triggers/TutorialTrigger.cs
+++ Assets/Scripts/Triggers/TutorialTrigger.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class TutorialTrigger : MonoBehaviour
 {
     public GameObject Text;
+    public AudioPlayer audioPlayer;
 
     private void Start()
     {
@@ -14,10 +15,12 @@ public class TutorialTrigger : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player") Text.SetActive(true);
+        audioPlayer.PlaySFX(27, 1, Random.Range(0.9f, 1.1f));
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         if(collision.tag == "Player") Text.SetActive(false);
+        audioPlayer.PlaySFX(27, 1, Random.Range(0.9f, 1.1f));
     }
 }
